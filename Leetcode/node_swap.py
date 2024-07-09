27b451d17107d47fc2a32124729244390c69f6ba
Gaurish17Sharma <gaurishsharma03@gmail.com>
2024-07-09 16:47:36 +0530

    stacks and queues

---
+++ Leetcode/node_swap.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def print_list(self):
+        output = []
+        current_node = self.head
+        while current_node is not None:
+            output.append(str(current_node.value))
+            current_node = current_node.next
+        print(" <-> ".join(output))
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+            new_node.prev = temp
+        self.length += 1
+        return True
+
+    def swap_pairs(self):
+        dummy = Node(0)
+        dummy.next = self.head
+        previous_node = dummy
+        while self.head and self.head.next is not None:
+            first_node = self.head
+            second_node = self.head.next
+            
+            previous_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+            second_node.prev = previous_node
+            first_node.prev = second_node
+            
+            if first_node.next is not None:
+                first_node.next.prev = first_node
+                
+            self.head = first_node.next
+            previous_node = first_node
+            
+        self.head = dummy.next
+
+my_dll = DoublyLinkedList(1)
+my_dll.append(2)
+my_dll.append(3)
+my_dll.append(4)
+
+print('my_dll before swap_pairs:')
+my_dll.print_list()
+
+my_dll.swap_pairs() 
+
+
+print('my_dll after swap_pairs:')
+my_dll.print_list()
---
+++ Leetcode/palindrome.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def is_palindrome(self):
+        if self.length == 0:
+            return True
+        if self.length == 1:
+            return True
+        else:
+            for _ in range(self.length):
+                if self.head.value == self.tail.value and self.head.next.value == self.tail.prev.value:
+                    return True
+                else:
+                    return False
+                self.head = self.head.next
+                self.tail = self.tail.prev
+            
+
+
+
+
+my_dll_1 = DoublyLinkedList(1)
+my_dll_1.append(2)
+my_dll_1.append(3)
+my_dll_1.append(2)
+my_dll_1.append(1)
+
+print('my_dll_1 is_palindrome:')
+print( my_dll_1.is_palindrome() )
+
+
+my_dll_2 = DoublyLinkedList(1)
+my_dll_2.append(2)
+my_dll_2.append(3)
+
+print('\nmy_dll_2 is_palindrome:')
+print( my_dll_2.is_palindrome() )
