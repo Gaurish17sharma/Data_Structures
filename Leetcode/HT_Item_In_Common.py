4ebefe27d411152e540e60e6c4822ce9da66fc95
Gaurish17Sharma <gaurishsharma03@gmail.com>
2024-07-13 15:57:30 +0530

    Leetcode/HT_Item_In_Common.py

---
+++ Leetcode/HT_Item_In_Common.py
@@ -0,0 +1,15 @@
+def item_in_common(list1,list2):
+    dict = {}
+    for i in list1:
+        dict[i] = True
+        
+    for j in list2:
+        if j in dict:
+            return True
+    return False
+
+list1 = [1,3,5]
+list2 = [2,4,5]
+
+
+print(item_in_common(list1, list2))
